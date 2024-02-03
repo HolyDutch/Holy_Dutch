1053b6b85009d2b0544ec0656f9f140cac06e921
Pauline Wambui <122151987+HolyDutch@users.noreply.github.com>
2024-02-03 10:33:11 +0300

    Add files via upload

---
+++ chess/piece.java
@@ -0,0 +1,39 @@
+import java.util.LinkedList;
+
+public class piece {
+    int xp,yp,x,y;
+    boolean iswhite;
+    LinkedList<piece> ps;
+    String name;
+    public piece (int xp, int yp, boolean iswhite, String n,LinkedList<piece> ps){
+        this.xp=xp;
+        this.yp=yp;
+        x=xp*64;
+        y=yp*64;
+        this.iswhite=iswhite;
+        this.ps=ps;
+        name=n;
+        ps.add(this);
+    }
+    public void move(int xp,int yp){
+
+        if(  chess1.getPiece(xp*64,yp*64 )!=null) {
+            if (chess1.getPiece(xp*64,yp*64 ).iswhite!=iswhite){
+                chess1.getPiece(xp * 64, yp * 64).kill();
+        }else {
+       x= this.xp*64;
+      y= this.yp*64;
+      return ;
+            }
+        }
+        this.xp=xp;
+        this.yp=yp;
+        x=xp*64;
+        y=yp*64;
+    }public void kill(){
+        ps.remove(this);
+    }
+}
+
+
+
